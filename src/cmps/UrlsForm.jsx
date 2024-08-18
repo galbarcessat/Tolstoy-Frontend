1808efe129645b62a22f24d3e53fb902e8f42f51
galbarcessat <galbarcessat1@gmail.com>
2024-08-18 16:35:29 +0300

    removed unsed files , UrlsForm design and basic functionallity

--- src/cmps/UrlsForm.jsx
+++ src/cmps/UrlsForm.jsx
@@ -1,42 +1,86 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, IconButton } from '@mui/material';
+import { Add as AddIcon, Delete as DeleteIcon } from '@mui/icons-material';
 
-export function UrlsForm() {
+export function UrlsForm({ onSubmit }) {
     const [urls, setUrls] = useState(['', '', ''])
 
     function handleUrlChange(index, value) {
         const newUrls = [...urls]
         newUrls[index] = value
         setUrls(newUrls)
-    };
+    }
 
     function handleAddUrl() {
         setUrls([...urls, ''])
-    };
+    }
+
+    function handleRemoveUrl(index) {
+        const newUrls = urls.filter((_, i) => i !== index);
+        setUrls(newUrls);
+    }
 
     function handleSubmit(event) {
         event.preventDefault()
         if (urls.some(url => !url)) {
-            // alert('Please fill out all URL fields.');
+            alert('Please fill out all URL fields.')
         } else {
             onSubmit(urls)
         }
-    };
+    }
 
     return (
-        <form >
-            {urls.map((url, index) => (
-                <div key={index}>
-                    <input
-                        type="url"
-                        value={url}
-                        onChange={(e) => handleUrlChange(index, e.target.value)}
-                        placeholder={`Enter URL ${index + 1}`}
-                        required
-                    />
-                </div>
-            ))}
-            <button type="button" onClick={handleAddUrl}>Add Another URL</button>
-            <button type="submit">Submit</button>
-        </form>
-    );
+        <div className='form-container'>
+            <h1>Get URLs Metadata :</h1>
+            <form onSubmit={handleSubmit}>
+
+                {urls.map((url, index) => {
+                    return (
+                        <div className='input-container' key={index}>
+                            <TextField
+                                fullWidth
+                                type='url'
+                                variant="outlined"
+                                sx={{ width: '330px' }}
+                                label={`Enter URL ${index + 1}`}
+                                value={url}
+                                onChange={(e) => handleUrlChange(index, e.target.value)}
+                                required
+                            />
+
+                            {index >= 3 && (
+                                <IconButton
+                                    onClick={() => handleRemoveUrl(index)}
+                                    color="error"
+                                    aria-label="remove"
+                                >
+                                    <DeleteIcon />
+                                </IconButton>
+                            )}
+                        </div>
+                    )
+                })}
+
+                <Button
+                    variant="contained"
+                    color="primary"
+                    sx={{ textTransform: 'none' }}
+                    endIcon={<AddIcon />}
+                    onClick={handleAddUrl}
+
+                >
+                    Add Another URL
+                </Button>
+
+                <Button
+                    type="submit"
+                    variant="contained"
+                    color="success"
+                    sx={{ textTransform: 'none' }}
+                >
+                    Submit
+                </Button>
+            </form>
+        </div>
+    )
 }
